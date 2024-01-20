7147da037050df2ec6aebb3578f936db72b54340
Alfredo Hernandez Noriega <fredo@PLXG3H07K9WRN.local>
2024-01-20 11:21:46 -0600

    api productos

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo')
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+let products = [
+  {
+    id:1,
+    name: 'Jeans',
+    price: '30usd'
+  },
+  {
+    id:2,
+    name: 'Tennis',
+    price: '30usd'
+  },
+  {
+    id:3,
+    name: 'Jacket',
+    price: '30usd'
+  },
+  {
+    id:4,
+    name: 'T-shirt',
+    price: '30usd'
+  },
+]
+
+app.get('/productos', (req, res) => {
+  res.json(products)
 })
-
-app.get('/saludo', (req, res) => {
-  res.send('Hola, ¡bienvenido a la ruta de saludo!')
-})
-
-app.get('/usuarios', (req, res) => {
-  const usuarios = ['Usuario1', 'Usuario2', 'Usuario3'];
-  res.json(usuarios)
-})
-
 app.get('/producto/:id', (req, res) => {
-  const productId = req.params.id
-  res.send(`Información del producto con ID ${productId}`);
+  const id = Number(req.params.id)
+  const product = products.find(product => product.id === id)
+  res.json(product)
+})
+
+app.post('/productos', jsonParser, (req, res) => {
+  const nuevoProducto = req.body
+  console.log(req)
+  nuevoProducto.id = products.length + 1
+  products.push(nuevoProducto)
+  res.status(201).json(nuevoProducto)
+})
+
+app.put('/productos/:id', jsonParser, (req, res) => {
+  const id = Number(req.params.id)
+  const index = products.findIndex(p => p.id === id);
+
+  if(index !== -1) {
+    products[index] = {...products[index], ...req.body}
+    res.json(products[index])
+  } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+  }
+})
+
+app.delete('/productos/:id', (req, res) => {
+  const id = Number(req.params.id)
+  products = products.filter(p => p.id !== id)
+
+  res.json({ message: `El producto con el id: ${id}, fue eliminado correctamente`, data: products })
 })
 
 app.listen(port, () => {
